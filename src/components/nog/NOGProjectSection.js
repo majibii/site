8be90095d7a000e57579c8991cbd989a8e802073
Marina <m.jacquet@eggon.fr>
2025-05-29 22:21:22 +0200

    Update NOGProjectSection.js

--- src/components/nog/NOGProjectSection.js
+++ src/components/nog/NOGProjectSection.js
@@ -14,7 +14,9 @@ const NOGProjectSection = () => {
       <div className="nog-content">
         <div className="nog-main-content">
           <h1 className="nog-title">
-            <span className="highlight">N.O.G PROJECT</span>
+            <span className="highlight">
+              N.O.G<br />PROJECT
+            </span>
           </h1>
           <p className="nog-description">
             A unique collection of legal prompts and agents, built for real-world legal practice. 
