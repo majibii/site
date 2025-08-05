39dfefe3cc576dee3bb4e15f4455652172d08d88
Marina <m.jacquet@eggon.fr>
2025-08-05 19:06:40 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,16 @@ import { LogoLink } from '../components/logo/LogoLink';
 import { Content } from '../components/content/Content';
 import { Hidden } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import Dither from '../components/background/Dither';
+import DisplacementSphere from '../components/background/DisplacementSphere';
+import { FooterText } from '../components/footer/FooterText';
 import { SocialIcons } from '../components/content/SocialIcons';
 import { SpeedDials } from '../components/speedDial/SpeedDial';
 import NOGProjectSection from '../components/nog/NOGProjectSection';
 import StorySection from '../components/story/StorySection';
 import CollectionSection from '../components/collection/CollectionSection';
 import LegalStackSection from '../components/legalStack/LegalStackSection';
+import { motion } from 'framer-motion';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
-import ParallaxSection from '../components/parallax/ParallaxSection';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -22,12 +23,12 @@ const useStyles = makeStyles(() => ({
     padding: 0,
     position: 'relative',
   },
-  ditherBackground: {
+  sphereBackground: {
     position: 'fixed',
     top: 0,
     left: 0,
     width: '100%',
-    height: '100vh',
+    height: '100%',
     zIndex: -999,
     pointerEvents: 'none',
   },
@@ -57,30 +58,15 @@ export const Home = () => {
   
   return (
     <div className={classes.root}>
-      <div className={classes.ditherBackground}>
-        <Dither
-          waveColor={[0.5, 0.5, 0.5]}
-          disableAnimation={false}
-          enableMouseInteraction={true}
-          mouseRadius={0.3}
-          colorNum={4}
-          waveAmplitude={0.3}
-          waveFrequency={3}
-          waveSpeed={0.05}
-        />
+      <div className={classes.sphereBackground}>
+        <DisplacementSphere />
       </div>
 
       <div className={classes.homeSection}>
-        <ParallaxSection offset={30} speed={0.3}>
-          <LogoLink />
-        </ParallaxSection>
-        <ParallaxSection offset={50} speed={0.5}>
-          <Content />
-        </ParallaxSection>
+        <LogoLink />
+        <Content />
         <Hidden smDown>
-          <ParallaxSection offset={20} speed={0.2}>
-            <SocialIcons />
-          </ParallaxSection>
+          <SocialIcons />
         </Hidden>
         <Hidden mdUp>
           <SpeedDials />
@@ -90,27 +76,19 @@ export const Home = () => {
       <ThemeToggle />
 
       <div className={classes.contentSection}>
-        <ParallaxSection offset={40} speed={0.4}>
-          <NOGProjectSection />
-        </ParallaxSection>
+        <NOGProjectSection />
       </div>
 
       <div className={classes.contentSection}>
-        <ParallaxSection offset={60} speed={0.6}>
-          <StorySection />
-        </ParallaxSection>
+        <StorySection />
       </div>
 
       <div className={classes.contentSection}>
-        <ParallaxSection offset={35} speed={0.35}>
-          <LegalStackSection />
-        </ParallaxSection>
+        <LegalStackSection />
       </div>
 
       <div className={classes.contentSection}>
-        <ParallaxSection offset={45} speed={0.45}>
-          <CollectionSection />
-        </ParallaxSection>
+        <CollectionSection />
       </div>
     </div>
   );
