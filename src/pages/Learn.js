19c6e2d211a19dc3e11055978390a75c14aec844
Marina <m.jacquet@eggon.fr>
2025-08-09 13:11:10 +0200

    Update Learn.js

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,44 +1,16 @@
 import React from 'react';
 import Header from '../components/header/Header';
+import DisplacementSphere from '../components/background/DisplacementSphere';
+import LearnSection from '../components/learn/learnsection';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
-import LeranSection from '../components/Learn/LearnSection';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    margin: 0,
-    padding: 0,
-    position: 'relative',
-    backgroundColor: theme.palette.background.default,
-    color: theme.palette.foreground.default,
-    minHeight: '100vh',
-  },
-  contentSection: {
-    width: '100%',
-    position: 'relative',
-    backgroundColor: 'transparent',
-    zIndex: 1,
-    margin: 0,
-    padding: 0,
-  }
-}));
 
 export const Learn = () => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.root}>
-      <Header />
-      <ThemeToggle />
-      
-      <div className={classes.contentSection}>
-        <LearnSection />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <DisplacementSphere />
+            <Header />
+            <LearnSection />
+            <ThemeToggle />
+        </div>
+    );
 };
-
-export default Learn;
