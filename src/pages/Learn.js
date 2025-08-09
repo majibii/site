b0500db1f7824aab7ba5a263bdda43c6dd68879b
Marina <m.jacquet@eggon.fr>
2025-08-09 13:35:28 +0200

    Update Learn.js

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../components/header/Header';
-import DisplacementSphere from '../components/background/DisplacementSphere';
 import LearnSection from '../components/learn/learnsection';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
 
 export const Learn = () => {
     return (
         <div>
-            <DisplacementSphere />
             <Header />
             <LearnSection />
             <ThemeToggle />
