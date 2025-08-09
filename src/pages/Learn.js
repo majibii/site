9068b5b21cdd3ff7c6ecc4adc6bd098ccb22665d
Marina <m.jacquet@eggon.fr>
2025-08-09 12:37:21 +0200

    Update Learn.js

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import { ThemeToggle } from '../components/theme/ThemeToggle';
-import LeranSection from '../components/noglab/LearnSection';
+import LeranSection from '../components/Learn/LearnSection';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
