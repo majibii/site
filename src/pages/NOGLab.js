5a766c22ff15778ab7eb7b8b0a03d1a07831af8e
Marina <m.jacquet@eggon.fr>
2025-08-09 17:46:44 +0200

    Update NOGLab.js

--- src/pages/NOGLab.js
+++ src/pages/NOGLab.js
@@ -4,6 +4,7 @@ import { ThemeToggle } from '../components/theme/ThemeToggle';
 import NOGLabSection from '../components/noglab/NOGLabSection';
 import { makeStyles } from '@material-ui/core/styles';
 
+// MODIFICATION Étape 2.1 : Styles identiques à la page eggon pour uniformiser le header
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -31,6 +32,7 @@ export const NOGLab = () => {
 
   return (
     <div className={classes.root}>
+      {/* Header identique à celui de la page eggon */}
       <Header />
       <ThemeToggle />
       
