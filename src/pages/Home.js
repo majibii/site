3d4734096bd954bcf817f52eb103d58c1e87c130
Marina <m.jacquet@eggon.fr>
2025-08-09 11:01:59 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,79 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { HelmetMeta } from "../app/HelmetMeta";
-import { ThemeProvider } from "../components/theme/ThemeProvider";
-import { CssBaseline } from "@material-ui/core";
-import { logCredits } from "../utils/logCredits";
-import { Home } from "../pages/Home";
-// Import direct au lieu de lazy pour éviter les problèmes avec react-snapshot
-import Resume from "../pages/Resume";
-import NOGLab from "../pages/NOGLab";
-import PageNotFound from "../pages/PageNotFound";
+import React from 'react';
+import Header from '../components/header/Header';
+import { LogoLink } from '../components/logo/LogoLink';
+import { Content } from '../components/content/Content';
+import { Hidden } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { SocialIcons } from '../components/content/SocialIcons';
+import { SpeedDials } from '../components/speedDial/SpeedDial';
+import NOGProjectSection from '../components/nog/NOGProjectSection';
+import CollectionSection from '../components/collection/CollectionSection';
+import LegalStackSection from '../components/legalStack/LegalStackSection';
+import { ThemeToggle } from '../components/theme/ThemeToggle';
 
-export const App = () => {
-    logCredits();
-    
-    return (
-        <ThemeProvider>
-            <CssBaseline />
-            <Router>
-                <HelmetMeta />
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/nog-lab" component={NOGLab} />
-                    <Route path="/resume" component={Resume} />
-                    <Route path="*" component={PageNotFound} />
-                </Switch>
-            </Router>
-        </ThemeProvider>
-    );
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    position: 'relative',
+  },
+  homeSection: {
+    minHeight: '100vh',
+    width: '100%',
+    position: 'relative',
+    backgroundColor: 'transparent',
+    zIndex: 1,
+    margin: 0,
+    padding: '80px 0 0 0', // Add top padding to account for fixed header
+    display: 'flex',
+    flexDirection: 'column',
+    '@media (max-width: 768px)': {
+      padding: '70px 0 0 0',
+    },
+  },
+  contentSection: {
+    width: '100%',
+    position: 'relative',
+    backgroundColor: 'transparent',
+    zIndex: 1,
+    margin: 0,
+    padding: 0
+  }
+}));
+
+export const Home = () => {
+  const classes = useStyles();
+  
+  return (
+    <div className={classes.root}>
+      <Header />
+      <div className={classes.homeSection}>
+        <LogoLink />
+        <Content />
+        <Hidden smDown>
+          <SocialIcons />
+        </Hidden>
+        <Hidden mdUp>
+          <SpeedDials />
+        </Hidden>
+      </div>
+
+      <ThemeToggle />
+
+      <div className={classes.contentSection}>
+        <NOGProjectSection />
+      </div>
+
+      <div className={classes.contentSection}>
+        <LegalStackSection />
+      </div>
+
+      <div className={classes.contentSection}>
+        <CollectionSection />
+      </div>
+    </div>
+  );
 };
