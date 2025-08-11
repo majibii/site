45ce9e74e20bbfc3287148e6638e17a464fc8550
Marina <m.jacquet@eggon.fr>
2025-08-11 14:00:24 +0200

    Update NOGLabSection.js

--- src/components/noglab/NOGLabSection.js
+++ src/components/noglab/NOGLabSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './NOGLabSection.css';
-import { useTranslation } from 'react-i18next';
 
 const NOGLabSection = () => {
   const { t } = useTranslation();
