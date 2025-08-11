50a4fbe92781dd28beab403a92a782af37b3491d
Marina <m.jacquet@eggon.fr>
2025-08-11 14:06:03 +0200

    Update learnsection.js

--- src/components/learn/learnsection.js
+++ src/components/learn/learnsection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import './learnsection.css';
-import { useTranslation } from 'react-i18next';
 
 const LearnSection = () => {
   const { t } = useTranslation();
